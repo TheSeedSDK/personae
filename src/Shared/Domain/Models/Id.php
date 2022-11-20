b803988afd37650880ddbc524f4481af5b1013fc
Unay Santisteban <usantisteban@othercode.es>
2022-11-20 21:39:42 +0100

    Feature/Added Shared Module. (#9)

---
+++ src/Shared/Domain/Models/Id.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheSeed\Shared\Domain\Models;
+
+use ComplexHeart\Domain\Model\ValueObjects\UUIDValue;
+
+/**
+ * Class ID
+ *
+ * @author Unay Santisteban <usantisteban@othercode.es>
+ * @package TheSeed\Shared\Domain\Models
+ */
+final class Id extends UUIDValue
+{
+
+}
