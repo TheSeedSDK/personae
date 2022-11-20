19d1f9cc2c3680b1eae0769d2b85eace3db3afc4
Unay Santisteban <usantisteban@othercode.es>
2022-11-20 14:52:43 +0100

    Feature/Pest Testing Framework installed.
    * Parallel Pest plugin installed.
    + Mockery Pest plugin installed.
    * Faker Pest plugin installed.
    * Phunctional installed.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_that_true_is_true()
-    {
-        $this->assertTrue(true);
-    }
-}
+test('asserts true is true', function () {
+    expect(true)->toBeTrue();
+});
